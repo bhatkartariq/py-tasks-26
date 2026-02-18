b27b0465628d1413c7fdb088fcfe465a74ee505c
bhatkartariq <bhatkartariq@eng.rizvi.edu.in>
2026-02-19 01:04:25 +0530

    Create task7.py

---
+++ task7.py
@@ -0,0 +1,15 @@
+#Aim: WAP to split a string using .split(). Try different seperators.
+text = input("Enter a string(with space,comma,dash,colon): ")
+
+print("\nSplit by space:")
+print(text.split())
+
+print("\nSplit by comma:")
+print(text.split(","))
+
+print("\nSplit by dash:")
+print(text.split("-"))
+
+print("\nSplit by colon:")
+print(text.split(":"))
+
